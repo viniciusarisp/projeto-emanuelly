dac637dd151161e2b0913ba5f66174939f2adabd
viniciusarisp <viniciusarisp@gmail.com>
2022-08-03 16:55:33 -0300

    ababluble

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ const btnMobile = document.getElementById('btn-mobile')
 
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
+ 
+  const header = document.getElementById('header') 
+  header.classList.toggle('active')
+
+  const headactive = header.classList.contains('active')   
+
   const nav = document.getElementById('nav') 
   nav.classList.toggle('active')
-  const active = nav.classList.contains('active')   
-  event.currentTarget.setAttribute('aria-expanded' , active)
-  if (active) { event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+
+  const navactive = nav.classList.contains('active')   
+
+  event.currentTarget.setAttribute('aria-expanded' , navactive)
+
+  if (navactive) { event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
   } else {
     event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
   }
